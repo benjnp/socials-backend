bdbbaba0d3437369eccbb2a86c131c73cebd778c
benjnp <benjnp@gmail.com>
2024-01-22 21:56:45 +0800

    Trying Sanity

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,12 @@
-export const schemaTypes = []
+import user from './user';
+import pin from './pin';
+import comment from './comment';
+import postedBy from './postedBy';
+import like from './like';
+import category from './category'
+
+// export default createSchema({
+//     name: 'default',
+//     types: schemaTypes.concat
+// })
+export const schemaTypes = [user, pin, comment, postedBy, like, category]
